3d2b098c6d54d0b9bf3b7b96a9eb48613995940c
Jacob Waller <jacob.waller@dteenergy.com>
2020-08-12 18:54:20 -0500

    WIP

---
+++ Honeybee.Test.Common/Models/CreateUserRequestModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Honeybee.Test.Common.Models
+{
+    public class CreateUserRequestModel
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+    }
+}
---
+++ Honeybee.Test.Common/Models/CreateUserResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Honeybee.Test.Common.Models
+{
+    public class CreateUserResponseModel
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public string Id { get; set; }
+        public string CreatedAt { get; set; }
+    }
+}
