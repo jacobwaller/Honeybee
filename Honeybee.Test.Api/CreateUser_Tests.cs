88af95e2161017daf291a00687c085a2348dbe53
Jacob Waller <jacobwaller@jacobwaller.com>
2020-08-13 09:47:08 -0500

    More WIP

---
+++ Honeybee.Test.Api/CreateUser_Tests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using Honeybee.Test.Common.Models;
+using Honeybee.Test.Common;
+
+namespace Honeybee.Test.Api
+{
+    public class CreateUser_Tests : BaseApi
+    {
+        public CreateUser_Tests() : base()
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
